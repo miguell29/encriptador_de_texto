6e437914b69f7936974bb05053c44c82981829ac
miguel <mfjuaarez@gmail.com>
2022-01-23 22:10:54 -0300

    añadidas las funciones de comparar entrada y encriptar

--- script.js
+++ script.js
@@ -7,14 +7,57 @@
 "u" es convertido para "ufat"
 Solo letras minusculas
 No se permite acentuación de palabras 
-*/
 
-/* Reglas de desencriptación: 
-"enter" es convertido para "e" 
-"imes" es convertido para "i"
-"ai" es convertido para "a"
-"ober" es convertido para "o"
-"ufat" es convertido para "u"
-Solo letras minusculas
-No se permite acentuación de palabras   
+// Reglas de desencriptación: 
+//"enter" es convertido para "e" 
+//"imes" es convertido para "i"
+//"ai" es convertido para "a"
+//"ober" es convertido para "o"
+//"ufat" es convertido para "u"
+//Solo letras minusculas
+//No se permite acentuación de palabras
+
 */
+var entrada = document.querySelector("#input-texto");
+var textoEntrada =entrada.value
+var botonEncriptar =document.querySelector("#btn-encriptar");
+var letraCambiada = "";
+var textoEncriptado ="";
+var boton = document.querySelector("button")
+    function compararEntrada(letra){
+        if (letra === "e"){
+           return letraCambiada ="enter";
+        }   else {
+            if (letra === "i"){
+                return letraCambiada ="imes";
+             }   else {
+                if (letra === "a"){
+                    return letraCambiada ="ai";
+                 }   else {
+                    if (letra === "o"){
+                        return letraCambiada ="ober";
+                     }   else if (letra === "u"){
+                        return letraCambiada ="ufat";
+                     }  else{
+                         letraCambiada=letra;
+                         return letraCambiada;
+                     }  
+                 }
+             }
+        }
+
+
+
+       
+   a }
+
+
+    function encriptar(){
+        textoEntrada = entrada.value;
+        for(var i = 0; i< textoEntrada.lenght; i++){
+            compararEntrada(textoEntrada[i]);
+            textoEncriptado = textoEncriptado +letraCambiada;
+            return textoEncriptado;
+        }
+        console.log(textoEncriptado);
+    }
